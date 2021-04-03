31a340ced22680721bc56fa0249cbff1e03a2023
Matej Klemen <matej.klemen1337@gmail.com>
2021-04-03 13:54:19 +0200

    Add cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Bridging natural language inference and paraphrasing.',
+    author='Matej Klemen',
+    license='MIT',
+)
