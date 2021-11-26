806254beccdf8318618e2957c5ac5c71ba85cd6f
Matej Klemen <matej.klemen1337@gmail.com>
2021-11-26 12:34:23 +0100

    Add MSCOCO dataset class

--- src/data/paraphrase.py
+++ src/data/paraphrase.py
@@ -5,6 +5,7 @@ from io import StringIO
 from typing import Union, Iterable, Optional
 from warnings import warn
 
+import pandas as pd
 import torch
 
 from src.data.nli import TransformersSeqPairDataset
@@ -171,3 +172,51 @@ class MRPCTransformersDataset(TransformersSeqPairDataset):
         encoded["labels"] = valid_label
 
         super().__init__(**encoded)
+
+
+class MSCOCOTransformersDataset(TransformersSeqPairDataset):
+    def __init__(self, path: str, tokenizer,
+                 max_length: Optional[int] = None, return_tensors: Optional[str] = None,
+                 reverse_order: Optional[bool] = False):
+        data = pd.read_csv(path, sep="\t")
+
+        self.imid1 = data["sentence1_image_id"].tolist()
+        self.imid2 = data["sentence2_image_id"].tolist()
+
+        self.seq1 = data["sentence1"].tolist()
+        self.seq2 = data["sentence2"].tolist()
+        valid_label = data["is_paraphrase"].tolist()
+
+        if reverse_order:
+            self.imid1, self.imid2 = self.imid2, self.imid1
+            self.seq1, self.seq2 = self.seq2, self.seq1
+
+        self.label_names = ["not_paraphrase", "paraphrase"]
+        self.label2idx = {curr_label: i for i, curr_label in enumerate(self.label_names)}
+        self.idx2label = {i: curr_label for curr_label, i in self.label2idx.items()}
+
+        # Sanity check: expecting binary task
+        assert len(set(valid_label)) == 2
+
+        optional_kwargs = {}
+        if return_tensors is not None:
+            valid_label = torch.tensor(valid_label)
+            optional_kwargs["return_tensors"] = "pt"
+
+        if max_length is not None:
+            optional_kwargs["max_length"] = max_length
+            optional_kwargs["padding"] = "max_length"
+            optional_kwargs["truncation"] = "longest_first"
+
+        encoded = tokenizer.batch_encode_plus(list(zip(self.seq1, self.seq2)), **optional_kwargs)
+        encoded["labels"] = valid_label
+
+        super().__init__(**encoded)
+
+
+if __name__ == "__main__":
+    from transformers import BertTokenizerFast
+
+    tokenizer = BertTokenizerFast.from_pretrained("bert-base-cased")
+    data = MSCOCOTransformersDataset("/home/matej/Documents/paraphrase-nli/experiments/MSCOCO_para/id_captions_train2014.tsv",
+                                     tokenizer=tokenizer)
