ee3f2c71a3e7d459df9b02756112c2f461a30659
Matej Klemen <matej.klemen1337@gmail.com>
2022-03-24 17:14:09 +0100

    Add script for training on TaPaCo

---
+++ experiments/TaPaCo_para/create_paraphrase_candidates.py
@@ -0,0 +1,50 @@
+import datasets
+import numpy as np
+import pandas as pd
+
+if __name__ == "__main__":
+	LANGS_TO_CONSIDER = ["en"]
+	pairs = []
+
+	for curr_lang in LANGS_TO_CONSIDER:
+		data = datasets.load_dataset("tapaco", curr_lang)["train"]
+		curr_pairs = {
+			"sentence1": [], "sentence2": [], "is_paraphrase": [], "language": []
+		}
+
+		# Group sentences by paraphrase set ID
+		set_to_sentences = {}
+		for ex in data:
+			if ex["paraphrase_set_id"] not in set_to_sentences:
+				set_to_sentences[ex["paraphrase_set_id"]] = [ex["paraphrase"]]
+			else:
+				set_to_sentences[ex["paraphrase_set_id"]].append(ex["paraphrase"])
+
+		paraphrases = list(set_to_sentences.items())  # list of tuples (ID_set, list_of_paras)
+
+		indices = np.arange(len(paraphrases))
+		for idx_set, (set_id, paras) in enumerate(paraphrases):
+
+			# 1. Create paraphrases by pairing adjacent sentences inside paraphrase set
+			num_para_pairs = len(paras) // 2
+			for _idx_pair in range(num_para_pairs):
+				curr_pairs["sentence1"].append(paras[_idx_pair * 2])
+				curr_pairs["sentence2"].append(paras[_idx_pair * 2 + 1])
+				curr_pairs["is_paraphrase"].append(1)
+				curr_pairs["language"].append(curr_lang)
+
+		print(f"{len(data)} examples in {len(set_to_sentences)} paraphrase sets")
+		pairs.append(pd.DataFrame(curr_pairs))
+
+	pairs = pd.concat(pairs).reset_index(drop=True)
+	print(f"{LANGS_TO_CONSIDER}: {pairs.shape[0]} candidates")
+
+	pairs.to_csv("tapaco_{}_paraphrase_candidates.tsv".format("_".join(LANGS_TO_CONSIDER)), sep="\t", index=False)
+
+
+
+
+
+
+
+
--- src/data/paraphrase.py
+++ src/data/paraphrase.py
@@ -10,8 +10,6 @@ import torch
 
 from src.data.nli import TransformersSeqPairDataset
 
-""" TODO: PIT2015? """
-
 
 class QQPTransformersDataset(TransformersSeqPairDataset):
     def __init__(self, path: Union[str, Iterable[str]], tokenizer,
@@ -213,10 +211,48 @@ class MSCOCOTransformersDataset(TransformersSeqPairDataset):
 
         super().__init__(**encoded)
 
+# TODO
+class TapacoTransformersDataset(TransformersSeqPairDataset):
+    def __init__(self, path: str, tokenizer,
+                 max_length: Optional[int] = None, return_tensors: Optional[str] = None,
+                 reverse_order: Optional[bool] = False):
+        data = pd.read_csv(path, sep="\t")
+
+        self.seq1 = data["sentence1"].tolist()
+        self.seq2 = data["sentence2"].tolist()
+        self.lang = data["language"].tolist()
+        valid_label = data["is_paraphrase"].tolist()
+
+        if reverse_order:
+            self.seq1, self.seq2 = self.seq2, self.seq1
+
+        self.label_names = ["not_paraphrase", "paraphrase"]
+        self.label2idx = {curr_label: i for i, curr_label in enumerate(self.label_names)}
+        self.idx2label = {i: curr_label for curr_label, i in self.label2idx.items()}
+
+        # Sanity check: expecting binary task
+        assert len(set(valid_label)) == 2
+
+        optional_kwargs = {}
+        if return_tensors is not None:
+            valid_label = torch.tensor(valid_label)
+            optional_kwargs["return_tensors"] = "pt"
+
+        if max_length is not None:
+            optional_kwargs["max_length"] = max_length
+            optional_kwargs["padding"] = "max_length"
+            optional_kwargs["truncation"] = "longest_first"
+
+        encoded = tokenizer.batch_encode_plus(list(zip(self.seq1, self.seq2)), **optional_kwargs)
+        encoded["labels"] = valid_label
+
+        super().__init__(**encoded)
+
 
 if __name__ == "__main__":
-    from transformers import BertTokenizerFast
+    from transformers import RobertaTokenizerFast
 
-    tokenizer = BertTokenizerFast.from_pretrained("bert-base-cased")
-    data = MSCOCOTransformersDataset("/home/matej/Documents/paraphrase-nli/experiments/MSCOCO_para/id_captions_train2014.tsv",
+    tokenizer = RobertaTokenizerFast.from_pretrained("roberta-base")
+    data = TapacoTransformersDataset("/home/matej/Documents/paraphrase-nli/experiments/TaPaCo_para/en/id_tapaco_en_train.tsv",
                                      tokenizer=tokenizer)
+
